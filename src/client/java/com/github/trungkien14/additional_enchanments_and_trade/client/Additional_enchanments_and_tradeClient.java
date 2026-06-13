86def522d185057737d3eff3d0af9d106c7ff154
trungkien14 <dinhtrungkien285@gmail.com>
2026-06-13 20:34:57 +0700

    Initial commit

---
+++ src/client/java/com/github/trungkien14/additional_enchanments_and_trade/client/Additional_enchanments_and_tradeClient.java
@@ -0,0 +1,10 @@
+package com.github.trungkien14.additional_enchanments_and_trade.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Additional_enchanments_and_tradeClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/github/trungkien14/additional_enchanments_and_trade/Additional_enchanments_and_trade.java
@@ -0,0 +1,10 @@
+package com.github.trungkien14.additional_enchanments_and_trade;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Additional_enchanments_and_trade implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
